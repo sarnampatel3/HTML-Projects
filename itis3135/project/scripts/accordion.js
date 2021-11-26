369772feac2a6546404fea701b70d0b4117c787c
sarnampatel3 <spate247@uncc.edu>
2021-11-26 13:22:12 -0500

    made changes

---
+++ itis3135/project/scripts/accordion.js
@@ -0,0 +1,18 @@
+//https://makitweb.com/accordion-with-html-css-and-jquery/
+$(document).ready(function(){
+    $(".accordion h1").click(function(){
+        var id = this.id;   /* getting heading id */
+    
+        /* looping through all elements which have class .accordion-content */
+        $(".accordion-content").each(function(){
+    
+            if($("#"+id).next()[0].id != this.id){
+                $(this).slideUp();
+            }
+    
+        });
+    
+        $(this).next().toggle();  /* Selecting div after h1 */
+    });
+    });
+    
