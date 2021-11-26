f1d44d11a928318196db5dce9783261e955be618
sarnampatel3 <spate247@uncc.edu>
2021-11-26 12:13:19 -0500

    edited js file

--- itis3135/Exercise/Activity16/Patel-Activity16.js
+++ itis3135/Exercise/Activity16/Patel-Activity16.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+    // https://www.flickr.com/services/feeds/docs/photos_public/
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=https://www.flickr.com/services/feeds/photos_public.gne&jsoncallback=?&tags=vectacorpbuilding";
+    
+    $.getJSON(url, function(data){
+        var writeHTML = "";
+        
+        $.each(data.items, function(i, item){
+            writeHTML += "<a href= \"" + item.media.m + "\"data-lightbox= \"buildingImages\" data-title=" 
+            + item.title + "><img src=" + item.media.m + "></a>";
+        });
 
+    $("#new_building").html(writeHTML);
+    });
 });
