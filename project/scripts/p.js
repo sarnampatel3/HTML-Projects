ac551b8309100647ae21e3f59f748aaaadfe7168
sarnampatel3 <spate247@uncc.edu>
2021-12-09 13:41:45 -0500

    xml to json

---
+++ project/scripts/p.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "players.json", //changed to json from xml
+        beforeSend: function() {
+            $("#players").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json", //changed to json from xml
+        success: function(data) {
+            $("#players").html("");
+            /*
+            $(data).find("management").children().each(function() {
+                var xmlDoc = $(this);
+                $("#team").append
+                ("<h3>" + xmlDoc.find("name").text() + "</h3>" +
+                          xmlDoc.find("title").text() + "<br>" +
+                          xmlDoc.find("bio").text() + "<br>");
+            });
+            */
+            $.each(data, function(){
+                $.each(this, function(key, value){
+                    $('#players').append(
+                        "<h3>" + value.name + "</h3>" +
+                        value.title + "<br>" +
+                        value.bio + "<br>" + 
+                        "<br>"
+                    );
+                });
+            });
+        }
+    });
+});
